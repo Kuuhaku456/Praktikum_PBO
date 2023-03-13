6953110a8837e7e811772984af82d1a5dea8b5f2
Kuuhaku456 <103502246+Kuuhaku456@users.noreply.github.com>
2023-03-13 12:47:50 +0800

    Add files via upload

---
+++ Posttest/src/Posttest_2/Anime.java
@@ -0,0 +1,59 @@
+package Posttest_2;
+
+class Anime {
+    private int tahun_rilis;
+    private String judul_anime;
+    private String Studio;
+    private String Produser;
+    private int jumlah_episode;
+    
+    Anime(String judul, int tahun_rilis, String Studio, String produser, int eps){
+        this.judul_anime = judul;
+        this.Studio = Studio;
+        this.tahun_rilis = tahun_rilis;
+        this.Produser = produser;
+        this.jumlah_episode = eps;
+    }
+    
+    public void SetJudul(String judul){
+        this.judul_anime = judul;
+    }
+    
+    public void setTahun(int tahun){
+        this.tahun_rilis = tahun;
+    }
+    
+    public void setStudio(String Studio){
+        this.Studio = Studio;
+    }
+    
+    public void setProduser (String Produser){
+        this.Produser = Produser;
+    }
+    
+    public void setjumlah_episode (int jumlah_eps){
+        this.jumlah_episode = jumlah_eps;
+    }
+    
+    public String getJudul(){
+        return judul_anime;
+    }
+
+    public String getStudio(){
+        return Studio;
+    }
+
+    public int getTahun(){
+        return tahun_rilis;
+    }
+
+    public String getproduser(){
+        return Produser;
+    }
+
+    public int getEpisode(){
+        return jumlah_episode;
+    }
+}
+
+
